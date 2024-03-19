710b515b97e02186ac3194d7fdb6497d3bb40c5d
zephyr <jafaribrownsha@gmail.com>
2024-03-18 23:01:27 -0400

    Trying to add tailwind

---
+++ Portfolio/src/App.jsx
@@ -0,0 +1,8 @@
+import HomePage from "./HomePage";
+import tailwindConfig from "../../tailwind.config";
+
+function App() {
+  return <HomePage></HomePage>;
+}
+
+export default App;
---
+++ Portfolio/src/HomePage.jsx
@@ -0,0 +1,9 @@
+function HomePage() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Welcome to my page</h1>
+    </>
+  );
+}
+
+export default HomePage;
